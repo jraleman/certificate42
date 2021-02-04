3696e94e82db4e7adb2452047ae4aa327fa7b6e1
jraleman <jraleman@bendiburg.com>
2021-02-04 14:12:30 -0800

    Modifies copy (text) of preact App function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import './style';
 export default function App() {
 	return (
 		<div>
-			<h1>Hello, World!</h1>
+			<h1>Certificate.42</h1>
+			<p>
+				Get your (un)official 42 Silicon Valley certificate here! 
+				Signed by Norminette and Moulinette, our dear and loved entities of this past institution.
+			</p>
+			<p>
+				This certficate is not valid, for anything, it's just to feel good about surviving the school.
+			</p>
+			<p>
+				Wanted to make this happen because there were some people expecting some kind of paper
+				after finishing school.
+			</p>
+			<p>
+				But you must be a 42 Silicon Valley student, I need to make sure of that. Sorry :(
+			</p>
 		</div>
 	);
 }
