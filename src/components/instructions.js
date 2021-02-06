7b14ee85c8dfb7531448f327a88428a2bf248def
jraleman <jraleman@bendiburg.com>
2021-02-05 18:19:41 -0800

    Creates Instructions component

---
+++ src/components/instructions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Instructions = () => (
+    <p>
+        Get your (un)official 42 Silicon Valley certificate here! 
+        Signed by Norminette and Moulinette, our dear and loved entities of this past institution.
+        <br />
+        This certficate is not valid, for anything, it's just to feel good about surviving the school.
+        <br />
+        Wanted to make this happen because there were some people expecting some kind of paper
+        after finishing school.
+        <br />
+        But you must be a 42 Silicon Valley student, I need to make sure of that. Sorry :(
+    </p>
+);
+
+export default Instructions;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import './style';
+import Instructions from './components/instructions';
 
-export default function App() {
-	return (
-		<div>
-			<h1>Certificate.42</h1>
-			<p>
-				Get your (un)official 42 Silicon Valley certificate here! 
-				Signed by Norminette and Moulinette, our dear and loved entities of this past institution.
-			</p>
-			<p>
-				This certficate is not valid, for anything, it's just to feel good about surviving the school.
-			</p>
-			<p>
-				Wanted to make this happen because there were some people expecting some kind of paper
-				after finishing school.
-			</p>
-			<p>
-				But you must be a 42 Silicon Valley student, I need to make sure of that. Sorry :(
-			</p>
-		</div>
-	);
-}
+const App = () => (
+	<div>
+		<h1>Certificate.42</h1>
+		<Instructions />
+	</div>
+);
+
+export default App;
