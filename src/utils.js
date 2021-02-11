e698aaeeae277b6576e4426cf59ff559a7962f44
jraleman <jraleman@bendiburg.com>
2021-02-10 23:01:10 -0800

    Implements funcs create and modify pdf

---
+++ src/utils.js
@@ -0,0 +1,29 @@
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+
+export async function modifyPdf() {
+    const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+    const pdfDoc = await PDFDocument.load(existingPdfBytes)
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+    const pages = pdfDoc.getPages()
+    const firstPage = pages[0]
+    const { width, height } = firstPage.getSize()
+    firstPage.drawText('This text was added with JavaScript!', {
+      x: 5,
+      y: height / 2 + 300,
+      size: 50,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(-45),
+    })
+    const pdfBytes = await pdfDoc.save();
+    return pdfBytes;
+};  
+
+export async function createPdf() {
+    const url = 'https://www.jraleman.com/certificate42/resources/42-certificate.pdf';
+    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
+    const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    return pdfDataUri
+}
